db455544a3bca9cb73885d03e58d781ec918c6ec
Preet Patel <patelpreet422@gmail.com>
2023-02-27 14:00:09 +0530

    use for loop to read all the incoming requests

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,16 @@
 use std::io::{Write, BufReader, BufRead};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
-use itertools::{Either, Itertools};
 
 fn handle_client(mut stream: TcpStream) {
     
     let buf_reader = BufReader::new(&stream);
 
-    // this will terminate the tcp-stream since we are reading all the lines in a tcp stream
-    // but the tcp stream only closed only when the server returns the response only so the client will keep the
-    // tcp-stream open 
-    let (http_request, _failures): (Vec<_>, Vec<_>) = buf_reader
+    let http_request: Vec<_> = buf_reader
         .lines()
-        .partition_map(|result| match result {
-            Ok(value) => Either::Left(value),
-            Err(err) => Either::Right(err)
-        });
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
 
     println!("Request: {:#?}", http_request);
     let response = format!("HTTP/1.1 200 OK\r\n\r\n");
@@ -28,23 +23,17 @@ fn handle_client(mut stream: TcpStream) {
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
-    loop {
-        let (stream, _) = listener.accept().unwrap();
-        thread::spawn(|| {
-            handle_client(stream);
-        });
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                // spawn a thread for each incoming request
+                std::thread::spawn(|| {
+                    handle_client(stream);
+                });
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
     }
-
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(stream) => {
-    //             std::thread::spawn(|| {
-    //                 handle_client(stream);
-    //             });
-    //         }
-    //         Err(e) => {
-    //             println!("Error: {}", e);
-    //         }
-    //     }
-    // }
 }
